54d08c1910cd39618f5473aba1936d861c72ed1e
galenegan <galenegan@gmail.com>
2026-03-10 16:38:48 -0400

    first pass at met module

--- src/atmosphere/met.py
+++ src/atmosphere/met.py
@@ -1,9 +1,6 @@
 import numpy as np
-import scipy.signal as sig
-from scipy.stats import linregress
 from typing import Optional, Union, List, Dict, Any, TypeAlias
 from src.utils.despike_utils import threshold, goring_nikora, recursive_gaussian
-from src.utils.spectral_utils import psd, csd, get_frequency_range
 from src.utils.base_instrument import BaseInstrument
 from src.utils.constants import (
     GRAVITATIONAL_ACCELERATION as g,
@@ -11,7 +8,7 @@ from src.utils.constants import (
     GAS_CONSTANT_DRY_AIR as R_a,
     GAS_CONSTANT_WATER_VAPOR as R_v,
     MOL_MASS_DRY_AIR as m_a,
-    MOL_MASS_WATER_VAPOR as m_v
+    MOL_MASS_WATER_VAPOR as m_v,
 )
 
 Numeric: TypeAlias = float | int | np.ndarray
@@ -26,6 +23,36 @@ class Met(BaseInstrument):
 
     Many of the methods are implemented based on their descriptions in Bradley & Fairall (2007). If a particular
     function/equation lacks a citation, it can likely be found in Appendix A therein.
+
+    Burst dictionary conventions
+    ----------------------------
+    Variables in a burst dict are assumed to be 2-D arrays of shape (n_heights, n_samples), where the
+    first axis corresponds to instrument heights (length self.n_heights) and the second axis is time.
+    The individual thermodynamic methods accept any Numeric type and broadcast over these arrays without
+    modification. Height information is always taken from self.z (shape (n_heights,)) rather than
+    from burst dict keys, so that self.z remains the single source of truth.
+
+    Standard burst dict keys recognised by :meth:`derive`:
+
+    Input keys
+        t         : air temperature (deg C)
+        p         : atmospheric pressure (mbar)
+        rh        : relative humidity (%)
+        salinity  : seawater salinity (PSU) -- optional, corrects vapor-pressure quantities
+
+    Output keys added by :meth:`derive`
+        e_s         : saturation vapor pressure (mbar)
+        e           : water vapor pressure (mbar)
+        rho_v       : water vapor density (kg/m^3)
+        w          : mixing ratio (kg/kg)
+        q           : specific humidity (kg/kg)
+        t_v         : virtual temperature (deg C)
+        rho_air     : moist air density (kg/m^3)
+        rho_air_dry : dry air density (kg/m^3)
+        cp          : specific heat capacity (J/(kg K))
+        L_v         : latent heat of vaporization (J/kg)
+        nu          : kinematic viscosity (m^2/s)
+        theta       : potential temperature (deg C)
 
     References
     ----------
@@ -91,7 +118,6 @@ class Met(BaseInstrument):
         # General validation
         BaseInstrument.validate_common_inputs(files, name_map, fs, z, data_keys)
 
-
     def set_preprocess_opts(self, opts: Dict[str, Any]):
         """
         Enable preprocessing for all subsequent burst loads using the options defined in the input dictionary.
@@ -152,30 +178,355 @@ class Met(BaseInstrument):
 
         return burst_data
 
-    def saturation_vapor_pressure(self, p: Numeric, t: Numeric, over_seawater: bool = True) -> Numeric:
+    def t_kelvin(self, t: Numeric) -> Numeric:
+        """Convert temperature from Celsius to Kelvin."""
+        return t + 273.15
+
+    def p_pascal(self, p: Numeric) -> Numeric:
+        """Convert pressure from millibar to Pascal."""
+        return p * 100
+
+    def saturation_vapor_pressure(self, t: Numeric, p: Numeric, salinity: Optional[Numeric] = None) -> Numeric:
         """
+        Saturation vapor pressure given pressure, temperature, and (optionally) seawater salinity
 
         Parameters
         ----------
-        p : Numeric
-            Atmospheric pressure in millibar
         t : Numeric
             Air temperature in Celcius
-        over_seawater : bool, optional
-            If true, the saturation vapor pressure is corrected with an assumed salinity 35 PSU
+        p : Numeric
+            Atmospheric pressure in millibar
+        salinity : Numeric, optional
+            If specified, the saturation vapor pressure is corrected to its "above seawater"
+            value using salinity in PSU
 
         Returns
         -------
-        Saturation vapor pressure in millibar
+        Numeric
+            Saturation vapor pressure in millibar
 
         """
         e_s = 6.1121 * (1.0007 + 3.46e-6 * p) * np.exp(17.502 * t / (240.97 + t))
-        if over_seawater:
-            return 0.981 * e_s
+        if salinity is not None:
+            return e_s * (1 - 5.37e-04 * salinity)
         else:
             return e_s
 
-    
+    def water_vapor_pressure(self, t: Numeric, p: Numeric, rh: Numeric, salinity: Optional[Numeric] = None) -> Numeric:
+        """
+        Water vapor pressure given temperature, pressure, relative humidity, and (optionally) seawater salinity
+
+        Parameters
+        ----------
+        t : Numeric
+            Air temperature in Celcius
+        p : Numeric
+            Atmospheric pressure in millibar
+        rh : Numeric
+            Relative humidity in %
+        salinity : Numeric, optional
+            If specified, the saturation vapor pressure is corrected to its "above seawater"
+            value using salinity in PSU
+
+        Returns
+        -------
+        Numeric
+            Water vapor pressure in millibar
+        """
+        e_s = self.saturation_vapor_pressure(t, p, salinity)
+        return (rh / 100) * e_s
+
+    def water_vapor_density(self, t: Numeric, p: Numeric, rh: Numeric, salinity: Optional[Numeric] = None) -> Numeric:
+        """
+        Water vapor density given temperature, pressure, relative humidity, and (optionally) seawater salinity
+
+        Parameters
+        ----------
+        t : Numeric
+            Air temperature in Celcius
+        p : Numeric
+            Atmospheric pressure in millibar
+        rh : Numeric
+            Relative humidity in %
+        salinity : Numeric, optional
+            If specified, the saturation vapor pressure is corrected to its "above seawater"
+            value using salinity in PSU
+
+        Returns
+        -------
+        Numeric
+            Water vapor density in kg/m^3
+        """
+        e = self.water_vapor_pressure(t, p, rh, salinity)
+        return 100 * e / (R_v * self.t_kelvin(t))
+
+    def mixing_ratio(self, t: Numeric, p: Numeric, rh: Numeric, salinity: Optional[Numeric] = None) -> Numeric:
+        """
+        Water vapor mixing ratio given temperature, pressure, relative humidity, and (optionally) seawater salinity
+
+        Parameters
+        ----------
+        t : Numeric
+            Air temperature in Celcius
+        p : Numeric
+            Atmospheric pressure in millibar
+        rh : Numeric
+            Relative humidity in %
+        salinity : Numeric, optional
+            If specified, the saturation vapor pressure is corrected to its "above seawater"
+            value using salinity in PSU
+
+        Returns
+        -------
+        Numeric
+            Mixing ratio in kg/kg
+        """
+        e = self.water_vapor_pressure(t, p, rh, salinity)
+        return 0.622 * e / (p - e)
+
+    def specific_humidity(self, t: Numeric, p: Numeric, rh: Numeric, salinity: Optional[Numeric] = None) -> Numeric:
+        """
+        Specific humidity given temperature, pressure, relative humidity, and (optionally) seawater salinity
+
+        Parameters
+        ----------
+        t : Numeric
+            Air temperature in Celcius
+        p : Numeric
+            Atmospheric pressure in millibar
+        rh : Numeric
+            Relative humidity in %
+        salinity : Numeric, optional
+            If specified, the saturation vapor pressure is corrected to its "above seawater"
+            value using salinity in PSU
+
+        Returns
+        -------
+        Numeric
+            Specific humidity in kg/kg
+        """
+        e = self.water_vapor_pressure(t, p, rh, salinity)
+        q = 0.622 * e / (p - 0.378 * e)
+        return q
+
+    def virtual_temperature(self, t: Numeric, p: Numeric, rh: Numeric, salinity: Optional[Numeric] = None) -> Numeric:
+        """
+        Virtual temperature given temperature, pressure, relative humidity, and (optionally) seawater salinity
+
+        Parameters
+        ----------
+        t : Numeric
+            Air temperature in Celcius
+        p : Numeric
+            Atmospheric pressure in millibar
+        rh : Numeric
+            Relative humidity in %
+        salinity : Numeric, optional
+            If specified, the saturation vapor pressure is corrected to its "above seawater"
+            value using salinity in PSU
+
+        Returns
+        -------
+        Numeric
+            Virtual temperature in Celcius
+        """
+        q = self.specific_humidity(t, p, rh, salinity)
+        t_v = t * (1 + 0.61 * q)
+        return t_v
+
+    def air_density(self, t: Numeric, p: Numeric, rh: Numeric) -> Numeric:
+        """
+        Moist air density given temperature, pressure, and relative humidity
+
+        Parameters
+        ----------
+        t : Numeric
+            Air temperature in Celcius
+        p : Numeric
+            Atmospheric pressure in millibar
+        rh : Numeric
+            Relative humidity in %
+
+        Returns
+        -------
+        Numeric
+            Moist air density in kg/m^3
+        """
+        e = self.water_vapor_pressure(t, p, rh)
+        p_dry = p - e
+        rho_air = (self.p_pascal(p_dry) * m_a + self.p_pascal(e) * m_v) / (R * self.t_kelvin(t))
+        return rho_air
+
+    def dry_air_density(self, t: Numeric, p: Numeric) -> Numeric:
+        """
+        Dry air density given temperature and pressure
+
+        Parameters
+        ----------
+        t : Numeric
+            Air temperature in Celcius
+        p : Numeric
+            Atmospheric pressure in millibar
+
+        Returns
+        -------
+        Numeric
+            Dry air density in kg/m^3
+        """
+        rho_air_dry = self.p_pascal(p) / (R_a * self.t_kelvin(t))
+        return rho_air_dry
+
+    def specific_heat(self, t: Numeric) -> Numeric:
+        """
+        Specific heat capacity of air at constant pressure
+
+        Parameters
+        ----------
+        t : Numeric
+            Air temperature in Celcius
+
+        Returns
+        -------
+        Numeric
+            Specific heat capacity in J/(kg K)
+        """
+        return 1005.6 + 0.0172 * t + 0.000392 * t**2
+
+    def latent_heat_of_vaporization(self, t: Numeric) -> Numeric:
+        """
+        Latent heat of vaporization
+
+        Parameters
+        ----------
+        t : Numeric
+            Air temperature in Celcius
+
+        Returns
+        -------
+        Numeric
+            Latent heat of vaporization in J/kg
+        """
+        return (2.501 - 0.00237 * t) * 1e6
+
+    def kinematic_viscosity(self, t: Numeric) -> Numeric:
+        """
+        Kinematic viscosity of air
+
+        Parameters
+        ----------
+        t : Numeric
+            Air temperature in Celcius
+
+        Returns
+        -------
+        Numeric
+            Kinematic viscosity in m^2/s
+        """
+        return 1.326e-5 * (1 + 6.542e-3 * t + 8.301e-6 * t**2 - 4.840e-9 * t**3)
+
+    def potential_temperature(self, t: Numeric, z: Numeric) -> Numeric:
+        """
+        Potential temperature, i.e. the temperature an air parcel would have if brought adiabatically
+        to a reference level at the surface
+
+        Parameters
+        ----------
+        t : Numeric
+            Air temperature in Celcius
+        z : Numeric
+            Height above the surface in meters. When called via :meth:`derive`, this is taken from
+            self.z and broadcast over the time dimension automatically.
+
+        Returns
+        -------
+        Numeric
+            Potential temperature in Celcius
+        """
+        cp = self.specific_heat(t)
+        theta = t + (g / cp) * z
+        return theta
+
+    def derive(self, burst_data: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+        """
+        Compute all thermodynamic quantities derivable from the variables present in a burst
+        dictionary, and return the burst dictionary augmented with those results.
+
+        Each quantity is computed only when all of its required inputs are available as keys in
+        ``burst_data``. The method never raises for missing inputs -- it simply skips any
+        quantities it cannot compute. Salinity is treated as optional throughout; when the
+        ``"salinity"`` key is present its value is forwarded to the vapor-pressure calculations.
+
+        Height is always taken from ``self.z`` (shape (n_heights,)) and is not read from
+        ``burst_data``. When computing potential temperature, ``self.z`` is reshaped to
+        (n_heights, 1) so that it broadcasts correctly against (n_heights, n_samples) arrays.
+
+        Input keys recognized
+        ----------------------
+        t         : air temperature (deg C),         shape (n_heights, n_samples)
+        p         : atmospheric pressure (mbar),     shape (n_heights, n_samples)
+        rh        : relative humidity (%),           shape (n_heights, n_samples)
+        salinity  : seawater salinity (PSU),         shape (n_heights, n_samples) -- optional
+
+        Output keys added to burst_data (all shape (n_heights, n_samples))
+        -------------------------------------------------------------------
+        e_s         : saturation vapor pressure (mbar)     -- requires t, p
+        rho_air_dry : dry air density (kg/m^3)              -- requires t, p
+        e           : water vapor pressure (mbar)          -- requires t, p, rh
+        rho_v       : water vapor density (kg/m^3)         -- requires t, p, rh
+        w          : mixing ratio (kg/kg)                  -- requires t, p, rh
+        q           : specific humidity (kg/kg)             -- requires t, p, rh
+        t_v         : virtual temperature (deg C)           -- requires t, p, rh
+        rho_air     : moist air density (kg/m^3)            -- requires t, p, rh
+        cp          : specific heat capacity (J/(kg K))     -- requires t
+        L_v         : latent heat of vaporization (J/kg)    -- requires t
+        nu          : kinematic viscosity (m^2/s)           -- requires t
+        theta       : potential temperature (deg C)         -- requires t (uses self.z)
+
+        Parameters
+        ----------
+        burst_data : dict
+            Burst dictionary whose keys are standard variable names (see above). Arrays are expected
+            to have shape (n_heights, n_samples). The dictionary is modified in-place and also
+            returned.
+
+        Returns
+        -------
+        dict
+            The input ``burst_data`` dictionary with derived quantities added as new keys.
+
+        """
+        t = burst_data.get("t")
+        p = burst_data.get("p")
+        rh = burst_data.get("rh")
+        salinity = burst_data.get("salinity")
+
+        has_t = t is not None
+        has_p = p is not None
+        has_rh = rh is not None
+
+        # Temperature-only quantities
+        if has_t:
+            burst_data["cp"] = self.specific_heat(t)
+            burst_data["L_v"] = self.latent_heat_of_vaporization(t)
+            burst_data["nu"] = self.kinematic_viscosity(t)
+            # self.z has shape (n_heights,); reshape to (n_heights, 1) to broadcast over time axis
+            burst_data["theta"] = self.potential_temperature(t, self.z.reshape(-1, 1))
+
+        # Temperature + pressure quantities
+        if has_t and has_p:
+            burst_data["e_s"] = self.saturation_vapor_pressure(t, p, salinity)
+            burst_data["rho_air_dry"] = self.dry_air_density(t, p)
+
+        # Temperature + pressure + relative humidity quantities
+        if has_t and has_p and has_rh:
+            burst_data["e"] = self.water_vapor_pressure(t, p, rh, salinity)
+            burst_data["rho_v"] = self.water_vapor_density(t, p, rh, salinity)
+            burst_data["w"] = self.mixing_ratio(t, p, rh, salinity)
+            burst_data["q"] = self.specific_humidity(t, p, rh, salinity)
+            burst_data["t_v"] = self.virtual_temperature(t, p, rh, salinity)
+            burst_data["rho_air"] = self.air_density(t, p, rh)
+
+        return burst_data
 
     @property
     def data_keys(self):
