2d590ff8589cb92885cf0b35d74d05a1882f77b6
galenegan <galenegan@gmail.com>
2026-03-12 17:10:45 -0400

    fixing empirical equations

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -6,6 +6,6 @@ MOL_MASS_DRY_AIR = 0.028965  # kg/mol
 MOL_MASS_WATER_VAPOR = 0.018015  # kg/mol
 
 # Seawater thermodynamics (TEOS-10)
-SSO = 35.16504        # standard ocean Absolute Salinity [g/kg]
-T0 = 273.15           # Celsius to Kelvin offset [K]
+SSO = 35.16504  # standard ocean Absolute Salinity [g/kg]
+T0 = 273.15  # Celsius to Kelvin offset [K]
 CP0 = 3991.86795711963  # specific heat of seawater at (SSO, 0 degC, 0 dbar) [J/(kg K)]
